063d9bf889ef7374f1a113996fe2351df69eaf64
Winnie Tong <tong.winnie@gmail.com>
2013-12-02 17:35:36 -0800

    UX additions
    Added hide and show info boxes when you hover over lists.

--- application.js
+++ application.js
@@ -83,22 +83,25 @@ $(document).ready(function(){
             var address = response.response.venues[i].location.address;
             var city =  response.response.venues[i].location.city;
             var state = response.response.venues[i].location.state;
+            var venueNameNS = venueName.replace(/[^a-zA-Z0-9-]/g, '');
 
-            $("#venueName").append("<h2><a href='" +URL+ "'>" + venueName + "</a><h2>" );
-            $("#venueName").append("<p>(" + category + ")</p>");
-            $("#venueName").append("<div class='address-field'><h3>"+ address + "</h3><h3>" + city + ", " + state + "</h3></div>");
-            $("#venueName").append("<div class='details-field'><p>Here Now: " + venueHereNow + "</p><p>Checkins: " + checkins + "</p></div>");
+            var venueCSS = "<div class='"+ venueNameNS + "'><a href='" + URL + "'><h2 class='venueName-field'>" + venueName + "</a></h2>";
+            var addressCSS = "<div class='address-field'><h3>"+ address + "</h3><h3>" + city + ", " + state + "</h3></div>";
+            var detailsCSS = "<div class='details-field'><p>(" + category + ")</p><p>Here Now: " + venueHereNow + "</p><p>Checkins: " + checkins + "</p></div>";
+            
 
-            drawMarkers(locations[i].lat, locations[i].lng, venueName, venueHereNow);
+            $("#venueName").append(venueCSS + addressCSS + detailsCSS);
+
+            drawMarkers(locations[i].lat, locations[i].lng, venueName, venueHereNow, venueNameNS);
         }
     }
 
-    function drawMarkers(lat, lng, venueName, venueHereNow){
+    function drawMarkers(lat, lng, venueName, venueHereNow, venueNameNS){
 
-        var contentString = venueName + ' (' + venueHereNow + ')';
+        var contentString = '<div style= "overflow: hidden;">' + venueName + ' (' + venueHereNow + ')</div>';
 
         var infowindow = new google.maps.InfoWindow({
-            content: contentString
+            content: contentString,
         });
 
         var markerOptions = {
@@ -113,6 +116,14 @@ $(document).ready(function(){
         google.maps.event.addListener(marker, 'mouseout', function() {
             infowindow.close(map,marker);
         });
+
+        $("." + venueNameNS).mouseenter(function(){
+            infowindow.open(map,marker);
+        });
+        $("." + venueNameNS).mouseleave(function(){
+            infowindow.close(map,marker);
+        });
+
     }
 
 });
